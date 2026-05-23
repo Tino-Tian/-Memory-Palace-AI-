99eaa1bdd7ab978917ac21d2c8baf8ffe2a32464
mac <mac@MacBookPro.lan>
2026-05-24 01:23:54 +0800

    修正 LICENSE 年份为 2026，更新 install 脚本中的年份

--- install.ps1
+++ install.ps1
@@ -256,7 +256,7 @@ INSERT OR IGNORE INTO categories (parent_id, name, path)
 @'
 MIT License
 
-Copyright (c) 2024
+Copyright (c) 2026
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- install.sh
+++ install.sh
@@ -238,7 +238,7 @@ INITSQL
 cat <<'LICENSE' > "$INSTALL_DIR/LICENSE"
 MIT License
 
-Copyright (c) 2024
+Copyright (c) 2026
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
